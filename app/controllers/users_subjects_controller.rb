274a8195424f49f475fdf01f7895e4a32def03f3
Konrad Janczyk <konrad.janczyk@polcode.net>
2015-04-15 14:59:34 +0200

    added ability to add and modify grades through modals, still need to move logic from grades to users_subjects

--- app/controllers/users_subjects_controller.rb
+++ app/controllers/users_subjects_controller.rb
@@ -1,22 +1,38 @@
-#its a UsersSubjects join table mostly - that's where the grades are stored
-class GradesController < ApplicationController
+class UsersSubjectsController < ApplicationController
   before_action :set_grade, only: [:show, :edit, :update]
   def index
     @grades = UsersSubject.where(user_id: params[:user], subject_id: params[:subject]).flatten
     @user = User.find(params[:user])
     @subject = Subject.find(params[:subject])
-    session[:subject] = @subject.id
-    session[:user] = @user.id
+
   end
+
   def edit
   end
+
   def show
   end
+
   def new
-  @grade = UsersSubject.new
+    @grade = UsersSubject.new
   end
+
   def create
-  @grade = UsersSubject.new
+    user = User.find(session[:user])
+    subject = Subject.find(session[:subject])
+    @grade = UsersSubject.create(value: grade_params[:value], subject: subject,
+    user: user)
+
+    respond_to do |format|
+      if @grade.save
+        format.json { head :no_content }
+        format.js
+      else
+        format.json { render json: @grade.errors.full_messages,
+                             status: :unprocessable_entity }
+      end
+
+    end
   end
 
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+
   root to: 'visitors#index'
 
   get 'grades/index', as: 'show_grades'
   get 'grades/:id/edit', to: 'grades#edit', as: 'edit_grade'
   patch 'grades/:id/update', to: 'grades#update', as: 'update_grade'
-  get 'grades/:id/show', to: 'grades#show', as: 'users_subject_path'
-
+  get 'grades/new', to: 'grades#new', as: 'new_grade'
+  resources :users_subjects
   devise_for :users
   resources :users
   resources :subjects
