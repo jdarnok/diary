5340bb0a431f51a4df47b832c7579dc9dd19ef2e
Konrad Janczyk <konrad.janczyk@polcode.net>
2015-05-05 13:55:37 +0200

    css rules change for navbar

--- app/controllers/websocket_controller.rb
+++ app/controllers/websocket_controller.rb
@@ -1,8 +1,6 @@
 class WebsocketController < WebsocketRails::BaseController
   def new_message
     # Here we call the rails-websocket broadcast_message method
-
     PdfGenerator.perform_async(message)
-    p 'butla'
   end
 end
--- app/workers/pdf_generator.rb
+++ app/workers/pdf_generator.rb
@@ -1,8 +1,7 @@
 class PdfGenerator
   include Sidekiq::Worker
-
   def perform(name)
     sleep(5)
+    pdf = Prawn::Document.new
     WebsocketRails[:pdf].trigger(:pdf_message,  "#{name} was completed")
-  end
 end
